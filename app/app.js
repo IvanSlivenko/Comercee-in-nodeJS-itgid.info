69b4dce65ccb7c8c22d2b0b498d1c475532913c5
IvanSlivenko <ivan8822@ulr.net>
2023-11-06 21:24:04 +0200

    06-11-2023-21-23

--- app/app.js
+++ app/app.js
@@ -1,5 +1,31 @@
-let a = [1, 2, "Hello", 3, 4, 5, 6, 7];
-console.log(a.length);
-for (let i = 0; i < a.length; i++) {
-  console.log(a[i]);
-}
+const http = require('http');
+const fs = require('fs');
+
+
+
+// http.createServer().listen(3000);
+http.createServer(function (request, respons) {
+  
+  // console.log(request.url);
+  // console.log(request.method);
+  // console.log(request.headers['user-agent']);
+  
+  respons.setHeader("Content-Type",
+    "text/html; charset=utf-8;")
+  
+  if (request.url == "/") {
+    respons.end("Main <b>Hello<b> Привіт");
+  } else if (request.url === "/cat")
+    respons.end("Category <h2> Hello <h2> Привіт з Категорії");
+  else if (request.url === "/dat") {
+    let myFile = fs.readFileSync('1.dat');
+    console.log(myFile);
+    respons.end(myFile);;
+  }
+
+  
+}).listen(3000);
+
+
+
+ 
