6d130b7c73485a230018de56c222a2f0cbfde45b
IvanSlivenko <ivan8822@ulr.net>
2023-11-11 22:39:43 +0200

    11-11-2023-22-39

--- app/app.js
+++ app/app.js
@@ -9,14 +9,53 @@ app.use(express.static('public'))
  */
 app.set('view engine', 'pug');
 
+/**
+ * Підключаємо mysql модуль
+ */
+let mysql = require('mysql');
+
+/**
+Налаштовуємо модуль 
+*/
+let con = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database:'market'
+  
+});
+
+
 app.listen(3000, function () {
   console.log('node express work on 3000');
 });
 
 app.get('/', function (req, res) {
-  res.render('main', {
-    foo: 'Hello',
-    bar: 7
+  
+  con.query(
+    "SELECT * FROM goods",
+    function (error, result) {
+    if (error) throw err;
+      // console.log(result);
+      let goods = {};
+      for (let i = 0; i < result.length; i++) { 
+        goods[result[i]['id']] = result[i];
+      }
+      // console.log(goods);
+      console.log(JSON.parse(JSON.stringify(goods)));
+      res.render("main", {
+        foo: "Hello",
+        bar: 7,
+        goods: JSON.parse(JSON.stringify(goods))
+      });
   });
+  
+});
+app.get('/cat', function (req,res) { 
+  console.log(reg.query.id);
+  let catId = req.query.id;
+
+  
+
 });
  
