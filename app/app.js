2a093aacc7e0fa1f1dcbd499548bafee093f15b8
IvanSlivenko <ivan8822@ulr.net>
2023-11-22 17:06:36 +0200

    22-11-2023-17-06

--- app/app.js
+++ app/app.js
@@ -1,18 +1,19 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-app.use(express.static('public'))
-
+app.use(express.static("public"));
 
 /**
- * 
+ *
  * Задаємо шаблонізатор
  */
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
 /**
  * Підключаємо mysql модуль
  */
-let mysql = require('mysql');
+let mysql = require("mysql");
+
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;  
 
 /**
 Налаштовуємо модуль 
@@ -24,81 +25,77 @@ let con = mysql.createPool({
   user: 'root',
   password: '',
   database:'market'
-  
+
 });
 
 /*
 Підключаємо nodemailer
 */
 
-const nodemailer = require('nodemailer')
-
+const nodemailer = require("nodemailer");
 
 app.listen(3000, function () {
-  console.log('node express work on 3000');
+  console.log("node express work on 3000");
 });
 
-app.get('/', function (req, res) {
-  
-  let cat = new Promise(function (resolve, reject) { 
+app.get("/", function (req, res) {
+  let cat = new Promise(function (resolve, reject) {
     con.query(
       "SELECT id, name, cost, image, category FROM (select id,name,cost,image,category, if(if(@curr_category != category, @curr_category := category, '') != '', @k := 0, @k := @k+1) as ind  FROM goods, (SELECT @curr_category := '') v ) goods WHERE ind < 3",
-        
-      function (error, result, fied) { 
-        if (error) return reject(error);
-        resolve(result);
-      }
-    );
-  })
-  
-  let catDescription = new Promise(function (resolve, reject) {
-    con.query(
-     "SELECT * FROM category",
+
       function (error, result, fied) {
         if (error) return reject(error);
         resolve(result);
       }
     );
   });
-  Promise.all([cat, catDescription]).then(function (value) { 
+
+  let catDescription = new Promise(function (resolve, reject) {
+    con.query("SELECT * FROM category", function (error, result, fied) {
+      if (error) return reject(error);
+      resolve(result);
+    });
+  });
+  Promise.all([cat, catDescription]).then(function (value) {
     console.log(value[1]);
     res.render("index", {
       goods: JSON.parse(JSON.stringify(value[0])),
-      cat: JSON.parse(JSON.stringify(value[1]))
+      cat: JSON.parse(JSON.stringify(value[1])),
     });
-  })
+  });
 });
 
-app.get('/cat', function (req, res) { 
+app.get("/cat", function (req, res) {
   console.log(req.query.id);
   let catId = req.query.id;
 
   let cat = new Promise(function (resolve, reject) {
     con.query(
-      'SELECT * FROM category WHERE id=' + catId,
+      "SELECT * FROM category WHERE id=" + catId,
       function (error, result) {
         if (error) reject(error);
         resolve(result);
-      });
+      }
+    );
   });
 
   let goods = new Promise(function (resolve, reject) {
     con.query(
-      'SELECT * FROM goods WHERE category=' + catId,
+      "SELECT * FROM goods WHERE category=" + catId,
       function (error, result) {
         if (error) reject(error);
         resolve(result);
-      });
+      }
+    );
   });
-  
+
   Promise.all([cat, goods]).then(function (value) {
     console.log(value[0]);
-    res.render('cat', {
+    res.render("cat", {
       cat: JSON.parse(JSON.stringify(value[0])),
-      goods: JSON.parse(JSON.stringify(value[1]))
+      goods: JSON.parse(JSON.stringify(value[1])),
     });
-  })
-  
+  });
 });
 
 app.get("/goods", function (req, res) {
@@ -108,16 +105,15 @@ app.get("/goods", function (req, res) {
     function (error, result, fields) {
       if (error) throw error;
       res.render("goods", { goods: JSON.parse(JSON.stringify(result)) });
-    });  
+    }
+  );
 });
 
-app.get('/order', function (req, res) {
-  
-    res.render("order");
-  
+app.get("/order", function (req, res) {
+  res.render("order");
 });
 
-app.post('/get-category-list', function (req, res) {
+app.post("/get-category-list", function (req, res) {
   // console.log(req.body);
   con.query(
     "SELECT id, category FROM category",
@@ -128,14 +124,14 @@ app.post('/get-category-list', function (req, res) {
     }
   );
 });
- 
+
 app.post("/get-goods-info", function (req, res) {
   console.log(req.body.key);
   if (req.body.key != 0) {
     con.query(
       "SELECT id,name,cost  FROM goods WHERE id IN (" +
-      req.body.key.join(",") +
-      ")",
+        req.body.key.join(",") +
+        ")",
       function (error, result, fields) {
         if (error) throw error;
         console.log(result);
@@ -146,40 +142,32 @@ app.post("/get-goods-info", function (req, res) {
         res.json(goods);
       }
     );
+  } else {
+    res.send("0");
   }
-  else { 
-    res.send('0');
-  }
-  
-  
-
-
 });
 
 // приймаємо запит
 app.post("/finish-order", function (req, res) {
   console.log(req.body);
-  if (req.body.key.length != 0) { 
+  if (req.body.key.length != 0) {
     let key = Object.keys(req.body.key);
     con.query(
-      "SELECT id,name,cost  FROM goods WHERE id IN (" +
-        key.join(",") +
-        ")",
+      "SELECT id,name,cost  FROM goods WHERE id IN (" + key.join(",") + ")",
       function (error, result, fields) {
         if (error) throw error;
         console.log(result);
-        sendMail(req.body, result).catch(console.error); 
-        res.send('1');
-      });
-
+        sendMail(req.body, result).catch(console.error);
+        saveOrder(req.body, result);
+        res.send("1");
+      }
+    );
+  } else {
+    res.send("0");
   }
-  else {
-    res.send('0')
-  }
-  
 });
 
-async function sendMail(data, result){
+async function sendMail(data, result) {
   let res = "<h2> Order in lite shop</h2>";
   let total = 0;
   for (let i = 0; i < result.length; i++) {
@@ -211,15 +199,23 @@ async function sendMail(data, result){
     from: "<ivan8822@ukr.net>",
     to: "ivan8822@ukr.net," + data.email,
     subject: "Lite shop order",
-    text: 'Hello world',
-    html: res
+    text: "Hello world",
+    html: res,
   };
 
   let info = await transporter.sendMail(mailOption);
   console.log("MessageSent: %s", info.messageId);
   console.log("PreviewSent: %s", nodemailer.getTestMessageUrl(info));
   return true;
-
 }
 
- 
+function saveOrder(data, result) { 
+  // data - інформація про користувача з  форми
+  // result - інформація про товар
+  let sql;
+  sql = "INSERT INTO user_info (user_name, user_phone, user_email, address) VALUES ('" + data.username + "','" + data.phone + "','" + data.email + "', '" + data.address + "')"
+  con.query(sql, function (error, result) {
+    if (error) throw error;
+    console.log('1 user info saved ');
+  });
+}
